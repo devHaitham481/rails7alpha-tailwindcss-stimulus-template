72eeefff4db38c1e33724241c2e78758eac2be46
devHaitham481 <51095418+devHaitham481@users.noreply.github.com>
2021-10-17 20:48:42 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+[![CircleCI](https://circleci.com/gh/Eric-Guo/tailwindcss-jit-stimulus/tree/main.svg?style=svg)](https://circleci.com/gh/Eric-Guo/tailwindcss-jit-stimulus/tree/main)
+
+# README
+
+This is a template with Rails 7.0, Webpacker 6.0.0, Tailwindcss JIT 2.2.7 and Stimulus 2.0.0.
+
+Need using Chrome to make JIT mode works, Safari 14.1.2 will failed to loading `application.css`.
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  mode: 'jit',
+  purge: [
+    './app/views/**/*.html.erb',
+    './app/views/**/*.html+phone.erb',
+    './app/helpers/**/*.rb',
+    './app/javascript/**/*.js',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Montserrat', 'Source Han Sans', 'Source Han Sans SC', 'system-ui', '-apple-system', 'BlinkMacSystemFont', 'Helvetica Neue', 'PingFang SC', 'Microsoft YaHei', 'Noto Sans CJK SC', 'WenQuanYi Micro Hei', 'sans-serif'],
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
+}
